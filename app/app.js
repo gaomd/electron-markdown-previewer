2a6f9a79e4d9fae4cda6073a3b250ca8b3ffa32c
Mengdi Gao <gaomdev@gmail.com>
2016-04-20 23:32:10 +0800

    Preview Markdown on drag & drop, and watch changes

--- app/app.js
+++ app/app.js
@@ -2,11 +2,11 @@
 // All stuff below is just to show you how it works. You can delete all of it.
 
 // Use new ES6 modules syntax for everything.
-import os from 'os'; // native node.js module
-import { remote } from 'electron'; // native electron module
-import jetpack from 'fs-jetpack'; // module loaded from npm
-import { greet } from './hello_world/hello_world'; // code authored by you in this project
-import env from './env';
+import {remote} from "electron";
+import jetpack from "fs-jetpack";
+import env from "./env";
+import fs from "fs";
+import commonmark from "commonmark"; // native node.js module
 
 console.log('Loaded environment variables:', env);
 
@@ -18,7 +18,63 @@ var appDir = jetpack.cwd(app.getAppPath());
 console.log('The author of this app is:', appDir.read('package.json', 'json').author);
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('greet').innerHTML = greet();
-    document.getElementById('platform-info').innerHTML = os.platform();
-    document.getElementById('env-name').innerHTML = env.name;
+    document.body.addEventListener('dragover', function (e) {
+        e.preventDefault();
+
+        // console.log('dragover', e.target);
+
+        if (!document.body.classList.contains('incoming-file')) {
+            document.body.classList.add('incoming-file');
+        }
+    });
+
+    document.body.addEventListener('dragleave', function (e) {
+        e.preventDefault();
+
+        // console.log('dragleave', e.target);
+
+        if (e.target.classList.contains('dropzone-mask')) {
+            document.body.classList.remove('incoming-file');
+        }
+    });
+
+    var watchingFilePath = null;
+
+    document.body.addEventListener('drop', function (e) {
+        e.preventDefault();
+
+        if (document.body.classList.contains('incoming-file')) {
+            document.body.classList.remove('incoming-file');
+        }
+
+        var file = e.dataTransfer.files[0];
+
+        console.log('File dragged:', file.path);
+
+        if (watchingFilePath !== null && watchingFilePath !== file.path) {
+            fs.unwatchFile(watchingFilePath);
+        }
+
+        fs.watchFile(file.path, function (curr, prev) {
+            if (curr.mtime !== prev.mtime) {
+                renderMarkdownFile(file.path, true);
+            }
+        });
+
+        renderMarkdownFile(file.path);
+    });
 });
+
+function renderMarkdownFile(path, isReload = true) {
+    var reader = new commonmark.Parser();
+    var writer = new commonmark.HtmlRenderer();
+    var parsed = reader.parse(fs.readFileSync(path, {encoding: 'UTF-8'})); // parsed is a 'Node' tree
+    var result = writer.render(parsed); // result is a String
+    var webContainer = document.getElementById('rendered');
+
+    if (isReload) {
+        var previousResult = webContainer.innerHTML;
+    }
+
+    webContainer.innerHTML = result;
+}
